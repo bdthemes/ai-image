01e3f3e1de1d9b271c46d6d9bcbabc49873844d5
Asikur Rahman <asikur@bdthemes.com>
2024-06-02 18:16:57 +0600

    ai-image style

--- src/generator/js/app.js
+++ src/generator/js/app.js
@@ -67,18 +67,48 @@
 						downloadBtns += `<button download class="btn btn-primary bdt-aimg-download-btn" data-url="${sources[key]}">Download ${key}</button>`;
 					}
 
+					// output += `
+					// 	<div class="card">
+					// 		<img src="${image.thumbnail}" class="card-img-top" alt="${image.photographer}">
+					// 		<div class="card-body">
+					// 			<h5 class="card-title
+					// 			">${image.photographer}</h5>
+					// 		</div>
+					// 		<div class="card-button-wrap">
+					// 			<a href="${image.url}" target="_blank" class="btn btn-primary">View</a>
+					// 			` + downloadBtns + `
+					// 		</div>
+					// 	</div>
+					// `;
 					output += `
-					<div class="col-6 col-md-4 col-lg-3 mb-4">
-						<div class="card">
-							<img src="${image.thumbnail}" class="card-img-top" alt="${image.photographer}">
-							<div class="card-body">
-								<h5 class="card-title
-								">${image.photographer}</h5>
-								<a href="${image.url}" target="_blank" class="btn btn-primary">View</a>
-								` + downloadBtns + `
+					<div class="card">
+					<img src="${image.thumbnail}" class="card-img-top" alt="${image.photographer}">
+						<div class="card-body">
+							<h5 class="card-title
+							">Tomáš Malík</h5>
+						</div>
+						<div class="card-button-wrap">
+							<a href="https://www.pexels.com/photo/steam-and-barren-hills-landscape-24778776/" target="_blank" class="btn btn-primary view-btn">View</a>
+							<button class="btn download-large">Download Large</button>
+						</div>
+						
+
+
+						<div class="dropdown" style="float:right;">
+							<button class="dropbtn">
+								<svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+								<path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M12 6h.01M12 12h.01M12 18h.01"/>
+						  	</svg>
+						  
+							</button>
+
+							<div class="dropdown-content">
+						    	<button download="" class="btn btn-primary bdt-aimg-download-btn" data-url="https://images.pexels.com/photos/24778776/pexels-photo-24778776.jpeg">Download original</button><button download="" class="btn btn-primary bdt-aimg-download-btn" data-url="https://images.pexels.com/photos/24778776/pexels-photo-24778776.jpeg?auto=compress&amp;cs=tinysrgb&amp;h=650&amp;w=940">Download large</button><button download="" class="btn btn-primary bdt-aimg-download-btn" data-url="https://images.pexels.com/photos/24778776/pexels-photo-24778776.jpeg?auto=compress&amp;cs=tinysrgb&amp;h=350">Download medium</button><button download="" class="btn btn-primary bdt-aimg-download-btn" data-url="https://images.pexels.com/photos/24778776/pexels-photo-24778776.jpeg?auto=compress&amp;cs=tinysrgb&amp;h=130">Download small</button>
 							</div>
 						</div>
-					</div>
+
+						</div>
+
 					`;
 				});
 
@@ -242,10 +272,11 @@
 				$(document).on('click', '.bdt-aimg-download-btn', function () {
 					App.downLoad(this);
 				});
-			}
+			},
+			
 		}
 
 		App.init();
-
 	});
+
 })(jQuery);
